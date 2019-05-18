dd198098a14581b80c444a704b9e51a6ffe0d2ba
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2019-05-18 12:29:04 +0300

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 31.945368, lng: 35.928371},
-    zoom: 10
-  });
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 31.945368, lng: 35.928371},
+//     zoom: 10
+//   });
 
-}
+// }
+
+
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html,body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
